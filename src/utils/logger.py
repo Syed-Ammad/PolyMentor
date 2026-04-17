aa204044198c10dd6727d66ca19d1dd41cc025fe
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-04-18 03:57:52 +0500

    logger

---
+++ src/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+
+def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    """Create and return a configured logger."""
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
